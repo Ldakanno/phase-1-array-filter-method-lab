35ed501b261d7ec183537db2c57794a1ffe99e22
Lianne Dakanno <liannedakanno@gmail.com>
2022-07-10 00:51:13 -0500

    commit tree 717dec9e06c3b84451975e42ac1b40a2f89f57d8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(array => array.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter(array => array.slice(0, letters.length) === letters);
+}
+
+function matchName(array, name) {
+    return array.filter(array => array.name.toLowerCase() === name.toLowerCase());
+}
